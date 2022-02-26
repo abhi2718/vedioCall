8fd50d38c8121aa6df908249e2fed2b95924b365
Abhishek Singh <abhiwebdev2718@gmail.com>
2022-02-26 15:23:45 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+      port = process.env.PORT ||7070,
+      path = require("path"),
+      app = express();
+      app.use(express.static(path.resolve(__dirname, './build')))
+      app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+      });
+      app.listen(port,()=> console.log(`listening on port ${port}`));
+     // https://speakingbookconnect.herokuapp.com/#987654?user
